cee37378babd5775a7723eddd19236897e6ab1d8
Kauan Gomes <kauamgpc@gmail.com>
2025-10-15 07:39:57 -0300

    Add files via upload

---
+++ jogo_de_batalha_naval_mestre_final.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+#define LINHA 10
+#define COLUNA 10
+
+void ql( int a ) { for ( int i = 0 ; i < a ; i++ ) { printf("\n"); } } // Função Para Printar Quebra de Linha na Tela.
+
+void label() { ql(2); printf("     Tabuleiro Batalha Naval :"); ql(5); } // Função Para Printar a Label na Tela.
+
+void espaco() { printf("           "); } // Função Para Printar Espaço na Tela Apenas Para Fins de Estetica.
+
+// Função Para Printar o Nome do Criador na Tela.
+void criador() { printf("© Criado Por: Kauan Gomes.  |  Github: ( https://github.com/KauanGomes-Lab ) © "); ql(2); }
+
+int main() {
+
+int r = 0 , g = 255 , b = 0; // Cores no Formato RGB.
+
+char letras [26]; // Variavel Char Para Armazenar as Letras de A à Z.
+
+for ( int i = 0 ; i < 26 ; i++ ) { letras[i] = 'A' + i; } // For Para Gerar as Letras de A à Z.
+
+int matriz [LINHA] [COLUNA]; // Variavel Inteira Para Armazenar a Matriz. 
+
+for ( int i = 0 ; i < LINHA ; i++ ) { 		// Estrutura de Repetição Para Gerar as Linhas da Matriz.
+    for ( int j = 0 ; j < COLUNA ; j++ ) { 	// Estrutura de Repetição Para Gerar as Colunas da Matriz.
+        matriz[i][j] = 0; 		// Atribuir o Valor Padrão 0 Para à Matriz.
+            
+	    // Cruz.
+            // Estrutura de Repetição Responsavel Por Gerar à Figura da Cruz Constituida de Valores 3 na Matriz.
+            for ( int i = 2 ; i < 3 ; i++ ) { 	
+            matriz[0][2] = 3;
+            	for ( int i = 2 ; i < 3 ; i++ ) { 
+            	matriz[1][i] = 3; }
+            	    for ( int i = 0 ; i < 5 ; i++ ) {
+            	    matriz[2][i] = 3; }
+			for ( int i = 2 ; i < 3 ; i++ ) { 
+			matriz[3][i] = 3; }
+			    for ( int i = 2 ; i < 3 ; i++ ) {
+			    matriz[4][i] = 3; }
+            		    }
+            			
+			    // Cone.
+			    // Estrutura de Repetição Responsavel Por Gerar à Figura do Cone Constituido de Valores 3 na Matriz.
+			    for ( int i = 2 ; i < 3 ; i++ ) { 
+			    matriz[7][i] = 3;
+				for ( int i = 1 ; i < 4 ; i++ ) {
+				matriz[8][i] = 3; }
+				    for ( int i = 0 ; i < 5 ; i++ ) {
+				    matriz[9][i] = 3; }
+				    }
+			
+				    // Octaedro.
+				    // Estrutura de Repetição Responsavel Por Gerar à Figura do Octaedro Constituido de Valores 3 na Matriz.
+				    for ( int i = 7 ; i < 8 ; i++ ) {
+				    matriz[3][i] = 3;
+					for ( int i = 6 ; i < 9 ; i++ ) {
+					matriz[4][i] = 3; }
+					    for ( int i = 7 ; i < 9 ; i++ ) {
+					    matriz[6][i] = 3; }
+						for ( int i = 5 ; i < 10 ; i++ ) {
+						matriz[5][i] = 3; }
+						    for ( int i = 6 ; i < 9 ; i++ ) {
+						    matriz[6][i] = 3; }
+							for ( int i = 7 ; i < 8 ; i++ ) {
+							matriz[7][i] = 3; }
+							}
+}
+}
+
+label(); // Chamar Função Label.
+
+espaco(); // Chamar Função Espaço.
+
+for ( int i = 0 ; i < 10 ; i++ ) { printf("%c  ", letras[i]); }	// Estrutura de Repetição Responsavel Por Printar Valores de Char.
+
+ql(2);	// Chamar Função Para Printar Quebra de Linha na Tela Apenas Para Fins de Estetica.
+
+// Estrutura de Repetição Responsavel Por Gerar o Número de Linhas na Tela.
+
+for ( int i = 0 ; i < LINHA ; i++ ) {
+   if ( i > 9 ) { printf("     %d    ", i); } // Condição da Estrutura de Repetição Se o Número de Linhas for Maior que 9 Ele Executa.
+   else { printf("     %d     ", i); } // Condição da Estrutura de Repetição Se o Número de Linhas for Menor que 9 Ele Executa.
+    
+    	// Estrutura de Repetição Responsavel Por Gerar o Número de Colunas na Tela.
+	for ( int j = 0 ; j < COLUNA ; j++ ) { 
+	   if (matriz[i][j] == 3) { printf("\033[38;2;%d;%d;%dm%d  \033[0m", r, g, b, matriz[i][j]); // Função Para dar Cor Verde aos Números 3.
+	   }else { printf("%d  ", matriz[i][j]); }
+	   }ql(1); // Chamar Função Para Printar Quebra de Linha na Tela Apenas Para Fins de Estetica.
+}
+
+ql(4); // Chamar Função Para Printar Quebra de Linha na Tela Apenas Para Fins de Estetica.
+
+criador();
+
+return 0;
+}
---
+++ jogo_de_batalha_naval_novato.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+
+int a = 11 , b = 10 , i , j;
+
+int  c , d;
+
+char sel; 
+
+char letras [10] = {'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J'};	
+
+int num [10] = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
+
+int matriz[a][b];
+
+for (i = 0; i < a; i++) {
+	for (j = 0; j < b; j++) {
+            matriz[i][j] = 0;
+            		matriz[3][1] = 3;
+			matriz[3][2] = 3;
+			matriz[3][3] = 3;
+			matriz[7][8] = 3;
+			matriz[8][8] = 3;
+			matriz[9][8] = 3;
+        }
+}
+
+printf("\n\n     Tabuleiro Batalha Naval :\n\n\n\n\n");
+
+printf("            ");
+
+for (int i = 0 ; i < 10 ; i++) {
+	printf("%c  ", letras[i]);
+}
+
+printf("\n\n");
+
+for (i = 1 ; i < a; i++) {
+    	if (i == 10) {
+    	printf("     %d     ",i);
+    	}else {
+    	printf("     %d      ",i);}
+        for (j = 0 ; j < b ; j++) {
+            printf("%d  ", matriz[i][j]);
+        }
+printf("\n");
+}
+printf("\n\n\n\n");
+return 0;
+}
