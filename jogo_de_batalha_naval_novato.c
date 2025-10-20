6542293acdc8548d2dca6836b0b0945fb86b3c0d
Kauan Gomes <kauamgpc@gmail.com>
2025-10-20 08:37:52 -0300

    Update jogo_de_batalha_naval_novato.c

--- jogo_de_batalha_naval_novato.c
+++ jogo_de_batalha_naval_novato.c
@@ -2,27 +2,25 @@
 
 int main() {
 
-int a = 11 , b = 10 , i , j;
+int r = 0 , g = 255 , b = 0;
 
-int  c , d;
+int linha = 10 , coluna = 10;
 
-char sel; 
+char letras [10] = { 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' };	
 
-char letras [10] = {'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J'};	
+int num [10] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 };
 
-int num [10] = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
+int matriz[linha][coluna];
 
-int matriz[a][b];
-
-for (i = 0; i < a; i++) {
-	for (j = 0; j < b; j++) {
+for (int i = 0; i < linha; i++) {
+	for (int j = 0; j < coluna; j++) {
             matriz[i][j] = 0;
             		matriz[3][1] = 3;
 			matriz[3][2] = 3;
 			matriz[3][3] = 3;
+			matriz[6][8] = 3;
 			matriz[7][8] = 3;
 			matriz[8][8] = 3;
-			matriz[9][8] = 3;
         }
 }
 
@@ -36,16 +34,21 @@ for (int i = 0 ; i < 10 ; i++) {
 
 printf("\n\n");
 
-for (i = 1 ; i < a; i++) {
+for (int i = 0 ; i < linha ; i++) {
     	if (i == 10) {
-    	printf("     %d     ",i);
-    	}else {
-    	printf("     %d      ",i);}
-        for (j = 0 ; j < b ; j++) {
-            printf("%d  ", matriz[i][j]);
-        }
+    	    printf("     %d     ", i);
+    		}else { printf("     %d      ", i); }
+        for (int j = 0 ; j < coluna ; j++) {
+           if (matriz[i][j] == 3) { 
+               printf("\033[38;2;%d;%d;%dm%d  \033[0m", r, g, b, matriz[i][j]); } // Função Para dar Cor Verde aos Números 3.
+	           else { printf("%d  ", matriz[i][j]); }
+	           }
+	
 printf("\n");
 }
 printf("\n\n\n\n");
+
+printf("© Criado Por: Kauan Gomes.  |  Github: ( https://github.com/KauanGomes-Lab ) © \n\n");
+
 return 0;
 }
